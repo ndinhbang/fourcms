09c1e91d9f100d87e9d4826a258d6441d49a162b
Nguyen Dinh Bang <bangnd@abilive.vn>
2022-12-02 11:44:57 +0700

    remove unused files

--- addons/abi/article/resources/js/cp.js
+++ addons/abi/article/resources/js/cp.js
@@ -1,16 +1,5 @@
-import ListingView from './components/Listing.vue'
-import View from './components/View.vue'
-import ArticleCreateForm from "./components/ArticleCreateForm";
-import ArticlePublishForm from "./components/ArticlePublishForm";
-
 Statamic.booting(() => {
-    Statamic.$components.register('article-view', View);
-    Statamic.$components.register('article-publish-form', ArticlePublishForm);
-    Statamic.$components.register('article-create-form', ArticleCreateForm);
-    Statamic.$components.register('article-list', ListingView);
-});
-// Statamic.$components.register('runway-listing-view', ListingView)
 
-console.log('holalala')
+});
 
 
